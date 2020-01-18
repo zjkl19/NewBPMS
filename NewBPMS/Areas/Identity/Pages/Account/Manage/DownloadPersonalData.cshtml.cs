20903aaa77cfc3bcfb407f2cfd5009f8b254bae6
迪南 林 <eamdfan@hotmail.com>
2020-01-18 16:53:58 +0800

    Change from IdentityUser to ApplicationUser

--- NewBPMS/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ NewBPMS/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -7,17 +7,18 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using NewBPMS.Models;
 using Newtonsoft.Json;
 
 namespace NewBPMS.Areas.Identity.Pages.Account.Manage
 {
     public class DownloadPersonalDataModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<DownloadPersonalDataModel> _logger;
 
         public DownloadPersonalDataModel(
-            UserManager<IdentityUser> userManager,
+            UserManager<ApplicationUser> userManager,
             ILogger<DownloadPersonalDataModel> logger)
         {
             _userManager = userManager;
@@ -36,7 +37,7 @@ namespace NewBPMS.Areas.Identity.Pages.Account.Manage
 
             // Only include personal data for download
             var personalData = new Dictionary<string, string>();
-            var personalDataProps = typeof(IdentityUser).GetProperties().Where(
+            var personalDataProps = typeof(ApplicationUser).GetProperties().Where(
                             prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
             foreach (var p in personalDataProps)
             {
