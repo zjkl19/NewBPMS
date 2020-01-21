43fb505f215a54bc58e149d81dabd68bb7221224
迪南 林 <eamdfan@hotmail.com>
2020-01-21 18:00:16 +0800

    Chinesize some text

--- NewBPMS/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ NewBPMS/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace NewBPMS.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "记住我?")]
             public bool RememberMe { get; set; }
         }
 
