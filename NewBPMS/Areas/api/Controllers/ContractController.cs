77f25adadd255799f102802d8b9383cd7f1a1352
迪南 林 <eamdfan@hotmail.com>
2020-02-01 15:53:22 +0800

    Adding api/Contract

---
+++ NewBPMS/Areas/api/Controllers/ContractController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewBPMS.IControllerServices;
+using NewBPMS.IRepository;
+using NewBPMS.ViewModels.ContractViewModels;
+
+namespace NewBPMS.Areas.api.Controllers
+{
+    [Area("api")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+        private readonly IUserManagerRepository _userManager;
+        private readonly IContractService _contractService;
+        private readonly IContractRepository _contractRepository;
+        private readonly IUserContractRepository _userContractRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public ContractController(
+            IMapper mapper,
+             IContractService contractService
+           , IContractRepository contractRepository
+           , IUserContractRepository userContractRepository
+           , IUserRepository userRepository
+            , IUserManagerRepository userManager)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _contractService = contractService;
+            _contractRepository = contractRepository;
+            _userContractRepository = userContractRepository;
+            _userRepository = userRepository;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [HttpGet]
+        public ActionResult<ContractCheckViewModel> Check()
+        {
+            var model = new ContractCheckViewModel
+            {
+                StatusMessage = StatusMessage,
+
+                ContractViewModels = _contractRepository.EntityItems
+                .Where(x => x.CheckStatus == (int)CheckStatus.NotChecked && x.SubmitStatus == (int)SubmitStatus.Submitted)
+                .Join(_userRepository.EntityItems, p => p.UserId, q => q.Id, (p, q) => _mapper.Map<ContractViewModel>(p))
+
+            };
+            return model;
+        }
+    }
+}
---
+++ NewBPMS/Areas/api/Controllers/TestContractController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewBPMS.Data;
+using NewBPMS.Models;
+
+namespace NewBPMS.Areas.api.Controllers
+{
+    [Area("api")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestContractController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestContractController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contracts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contract>>> GetContracts()
+        {
+            return await _context.Contracts.ToListAsync();
+        }
+
+        // GET: api/Contracts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contract>> GetContract(Guid id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return contract;
+        }
+
+        // PUT: api/Contracts/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContract(Guid id, Contract contract)
+        {
+            if (id != contract.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contract).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContractExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contracts
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Contract>> PostContract(Contract contract)
+        {
+            _context.Contracts.Add(contract);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContract", new { id = contract.Id }, contract);
+        }
+
+        [NonAction]
+        // DELETE: api/Contracts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Contract>> DeleteContract(Guid id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+
+            return contract;
+        }
+
+        private bool ContractExists(Guid id)
+        {
+            return _context.Contracts.Any(e => e.Id == id);
+        }
+    }
+}
