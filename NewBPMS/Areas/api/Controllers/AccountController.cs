0c13846bc6961478c274905cbfa37d83e5f696b0
迪南 林 <eamdfan@hotmail.com>
2020-02-04 20:49:15 +0800

    add api/Account

---
+++ NewBPMS/Areas/api/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using NewBPMS.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NewBPMS.Areas.api.Controllers
+{
+    [Area("api")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger _logger;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Password, bool RememberMe = false)
+        {
+
+            var result = await _signInManager.PasswordSignInAsync(Email, Password, RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User logged in.");
+                return Ok();
+            }
+            else
+            {
+                //ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                //return View(model);
+                return BadRequest();
+            }
+
+        }
+    }
+}
